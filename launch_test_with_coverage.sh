357a257347f4223f9ef77b633f0f6bb9f3af4733
rigoudyg <gaelle.rigoudy@meteo.fr>
2025-04-04 10:45:02 +0200

    Create a function to update parser to deal with config values. Change examples to use it.

--- launch_test_with_coverage.sh
+++ launch_test_with_coverage.sh
@@ -6,12 +6,16 @@ coverage erase
 coverage run
 # To be moved before once tests are fixed
 set -e
-# coverage run --parallel-mode scripts/database_transformation.py --output_dir="test" --dreq_export_version="raw"
-coverage run --parallel-mode scripts/database_transformation.py --output_dir="test" --dreq_export_version="release"
 coverage run --parallel-mode scripts/workflow_example.py
-rm -f "requested_v1.0.json" "requested_raw.json"
-# coverage run --parallel-mode scripts/workflow_example_2.py --output_dir="test" --dreq_export_version="raw"
-coverage run --parallel-mode scripts/workflow_example_2.py --output_dir="test" --dreq_export_version="release"
+rm -f "requested_v1.1.json" "requested_raw.json"
+coverage run --parallel-mode scripts/database_transformation.py --test --export="raw" --version="v1.0"
+coverage run --parallel-mode scripts/database_transformation.py --test --export="release" --version="v1.0"
+coverage run --parallel-mode scripts/workflow_example_2.py --test --export="raw" --version="v1.0"
+coverage run --parallel-mode scripts/workflow_example_2.py --test --export="release" --version="v1.0"
+coverage run --parallel-mode scripts/database_transformation.py --test --export="raw" --version="v1.1"
+coverage run --parallel-mode scripts/database_transformation.py --test --export="release" --version="v1.1"
+coverage run --parallel-mode scripts/workflow_example_2.py --test --export="raw" --version="v1.1"
+coverage run --parallel-mode scripts/workflow_example_2.py --test --export="release" --version="v1.1"
 
 coverage combine
 
