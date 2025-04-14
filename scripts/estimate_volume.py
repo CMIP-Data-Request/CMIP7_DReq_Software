a5232770658ba9f15e9259d4899a9a03e3289915
James Anstey <james.anstey@ec.gc.ca>
2025-04-14 07:10:39 +0000

    estimated data volume for all experiments and variables, or subsets

--- scripts/estimate_volume.py
+++ scripts/estimate_volume.py
@@ -11,24 +11,6 @@ import data_request_api
 import data_request_api.content.dreq_content as dc
 import data_request_api.query.dreq_query as dq
 
-def parse_args():
-    '''
-    Parse command-line arguments
-    '''
-
-    parser = argparse.ArgumentParser(
-        description='Estimate volume of requested model output'
-    )
-    # Positional arguments
-    parser.add_argument('requested_vars', type=str,
-                        help='json file specifying variables requested by experiment' +
-                        '(output from export_dreq_lists_json)')
-    # Optional arguments
-    parser.add_argument('-v', '--variables', nargs='+', type=str,
-                        help='include only the specified variables in the estimate')
-    parser.add_argument('-c', '--config_size', type=str, default='size.yaml',
-                        help='config file (yaml) giving size parameters to use in the volume estimate')
-    return parser.parse_args()
 
 def file_size_str(size):
     '''
@@ -56,24 +38,31 @@ def file_size_str(size):
     return sa + ' ' + su
 
 
-def get_variable_size(var_info, config, dreq_tables):   # MOVE INTO DREQ_QUERY
+def get_variable_size(var_info, config, dreq_tables):
     '''
     Return size (B) of 1 year of a variable.
     Also return a dict giving its dimension sizes (no. of gridpoints, with the time size being for 1 year).
     '''
 
+    # Available frequencies
+    freqs = [rec.name for rec in dreq_tables['frequency'].records.values()]
     days_per_year = 365
     freq_times_per_year = {
-        # 'subhr': days_per_year*48,
+        'subhr': days_per_year*48,
+        '1hr': days_per_year*24,
         '3hr': days_per_year*8,
         '6hr': days_per_year*4,
         'day': days_per_year,
         'mon': 12,
         'yr': 1,
         'dec': 0.1,
+        'fx': 1,
     }
+    if set(freq_times_per_year.keys()) != set(freqs):
+        raise Exception('Times per year must be defined for all available frequencies')
 
-    time_dims = ['time-intv', 'time-point']   # NEEDS TO BE COMPLETE
+    # Available time dimensions ('time-intv', 'time-point', etc)
+    time_dims = [rec.name for rec in dreq_tables['temporal shape'].records.values()]
 
     dimensions = var_info['dimensions']
     if isinstance(dimensions, str):
@@ -82,6 +71,7 @@ def get_variable_size(var_info, config, dreq_tables):   # MOVE INTO DREQ_QUERY
 
     dim_sizes = {}
     for dim in dimensions:
+        n = None
         if dim in time_dims:
             # Get number of time gridpoints in one year
             frequency = var_info['frequency']
@@ -91,14 +81,13 @@ def get_variable_size(var_info, config, dreq_tables):   # MOVE INTO DREQ_QUERY
             n = config['dimensions'][dim]
         else:
             # Use requested dimension size (i.e., size given in the data request)
-            
             rec = dreq_tables['coordinates and dimensions'].get_attr_record('name', dim, unique=True)
             # print(rec)
             if hasattr(rec, 'size'):
                 n = rec.size
             else:
                 n = 1
-
+                # raise ValueError(f'Unknown size for dimension: {dim}')
         dim_sizes[dim] = n
 
     num_gridpoints = 1
@@ -112,6 +101,32 @@ def get_variable_size(var_info, config, dreq_tables):   # MOVE INTO DREQ_QUERY
     return size, dim_sizes
 
 
+def parse_args():
+    '''
+    Parse command-line arguments
+    '''
+
+    parser = argparse.ArgumentParser(
+        description='Estimate volume of requested model output'
+    )
+    # Positional arguments
+    parser.add_argument('requested_variables', type=str,
+                        help='json file specifying variables requested by experiment' +
+                        ' (output from export_dreq_lists_json, which specifies the data request version)')
+    # Optional arguments
+    parser.add_argument('-v', '--variables', nargs='+', type=str,
+                        help='include only the specified variables in the estimate')
+    parser.add_argument('-e', '--experiments', nargs='+', type=str,
+                        help='include only the specified experiments in the estimate')
+    parser.add_argument('-c', '--config-size', type=str, default='size.yaml',
+                        help='config file (yaml) giving size parameters to use in the volume estimate')
+    parser.add_argument('-o', '--outfile', type=str,
+                        help='name of output file, default: volume_estimate_{data request version}.json')
+    parser.add_argument('-vso', '--variable-size-only', action='store_true',
+                        help='show ONLY the sizes of individual variables (ignores experiments)')
+    return parser.parse_args()
+
+
 def main():
 
     args = parse_args()
@@ -122,16 +137,25 @@ def main():
         print('Loaded ' + config_file)
 
     warning_msg = '\n * * * WARNING * * *'
-    warning_msg += '\n These volumes are only an estimate, and should be verified against known data volumes\n'
+    warning_msg += '\n These volumes are an initial estimate, to be improved in the next software version.'
+    warning_msg += '\n They should be used with caution and verified against known data volumes.\n'
 
-    filepath = args.requested_vars
+    filepath = args.requested_variables
     with open(filepath, 'r') as f:
         requested = json.load(f)
         print('Loaded ' + filepath)
     use_dreq_version = requested['Header']['dreq content version']
     print(f'Estimating volume for data request {use_dreq_version}')
 
+    if not args.outfile:
+        outfile = f'volume_estimate_{use_dreq_version}.json'
+    else:
+        outfile = args.outfile
+
     expts = requested['Header']['Experiments included']
+    if args.experiments:
+        # Only retain specified experiments
+        expts = [expt for expt in expts if expt in args.experiments]
     vars_by_expt = requested['experiment']
 
     # Download specified version of data request content (if not locally cached)
@@ -145,16 +169,19 @@ def main():
         'coordinates and dimensions': base['Coordinates and Dimensions'],
         'expts': base['Experiments'],
         'opps': base['Opportunity'],
+        'temporal shape': base['Temporal Shape'],
+        'frequency': base['CMIP7 Frequency'],
     }
 
     # Get metadata for variables
     variables = dq.get_variables_metadata(
         base,
-        use_dreq_version
+        use_dreq_version,
+        compound_names=args.variables,
     )
 
-    if args.variables:
-        # Find size of 1 year of specified variables, then exit
+    if args.variables and args.variable_size_only:
+        # Find size of specified variables, then exit
         for var_name in args.variables:
             var_info = variables[var_name]
             size, dim_sizes = get_variable_size(var_info, config, dreq_tables)
@@ -170,80 +197,76 @@ def main():
             dim_str = ', '.join([f'{k}={v}' for k,v in dim_sizes.items()])
             msg += f' (dimension sizes for 1 year: {dim_str})'
             print(msg)
-
         print(warning_msg)
         sys.exit()
 
-
-
-    # print(dreq_tables['expts'])
-
-    # print(dreq_tables['expts'].get_record(0))
-
     expt_records = {expt_rec.experiment: expt_rec for expt_rec in dreq_tables['expts'].records.values()}
-
-    # for expt in sorted(expt_records, key=str.lower):
+    expt_size = OrderedDict()
     for expt in expts:
         expt_rec = expt_records[expt]
-
-        print('%-5s' % expt_rec.size_years_minimum, expt)
-
-        request_size = {}
+        # print('%-5s' % expt_rec.size_years_minimum, expt)
+        request_size = OrderedDict()
         for priority, var_list in vars_by_expt[expt].items():
-            # print(priority)
-            # print(var_list, len(var_list))
-            request_size[priority] = {'size' : 0, 'no. of vars' : len(var_list)}
+            if args.variables:
+                # Only retain specified variables from the list of requested  variables
+                var_list = [var_name for var_name in var_list if var_name in args.variables]
+            request_size[priority] = OrderedDict({
+                'no. of vars': len(var_list),
+                'size': 0, 
+                })
             for var_name in var_list:
                 var_info = variables[var_name]
-
-                try:
-                    size, dim_sizes = get_variable_size(var_info, config, dreq_tables)  # FIX
-                except:
-                    size = 1
-
+                # Get size of 1 year of this variable
+                size, dim_sizes = get_variable_size(var_info, config, dreq_tables)
+                if var_info['frequency'] == 'fx':
+                    pass
+                # ALSO NEED TO HANDLE CLIMATOLOGIES
+                else:
+                    # Multiply by minimum number of request years for this experiment
+                    size *= expt_rec.size_years_minimum
+                # Increment size tally for this experiment at this priority level
                 request_size[priority]['size'] += size
 
         priority = 'TOTAL'    
         assert priority not in request_size
-        request_size[priority] = {
+        request_size[priority] = OrderedDict({
+            'no of vars': sum([d['no. of vars'] for d in request_size.values()]),
             'size': sum([d['size'] for d in request_size.values()]),
-            'no of vars': sum([len(var_list) for var_list in vars_by_expt[expt].values()])
-        }
+            })
+        for d in request_size.values():
+            d['size_str'] = file_size_str(d['size'])
 
-        for priority in request_size:
-            size = request_size[priority]['size']
-            request_size[priority]['size_str'] = file_size_str(size)
+        expt_size[expt] = OrderedDict({
+            'no. of years': expt_rec.size_years_minimum,
+            'no. of ensemble members': 1,  # ADD ENSEMBLE MEMBER INFO LATER!
+        })
+        # if args.variables:
+        #     expt_size[expt]['variables subset of request'] = args.variables
+        expt_size[expt].update({
+            'total request size (all priorities)': request_size['TOTAL'],
+            'request size by priority level': OrderedDict(),
+        })
+        for priority in vars_by_expt[expt]:
+            expt_size[expt]['request size by priority level'][priority] = request_size[priority]
 
-        print(expt, request_size)
-        stop
-            
+    out = OrderedDict({
+        'Header': OrderedDict({
+            'dreq content version': use_dreq_version,
+            'requested experiments and variables': args.requested_variables,
+            'model-specific size options': args.config_size,
+        }),
+        'volume by experiment': expt_size,
+    })
+    if args.variables:
+        out['Header']['variables subset of request'] = args.variables
+    if args.experiments:
+        out['Header']['experiments subset of request'] = args.experiments
 
+    with open(outfile, 'w') as f:
+        json.dump(out, f, indent=4)
+        print('Wrote ' + outfile)
 
-
-        # show size per expt as function of priority level
-
-        break
-
-    # print(dreq_tables['var groups'].get_record(0))
-
-    # print(dreq_tables['opps'].get_record(0))
-    '''
-    minimum_ensemble_size: 1
-    other ensem info?
-
-    give size per ensem member  
-    
-    add ensem member info to vars list json, b/c depends on opportunity
-
-    prob cannot account for timeslices
-
-    get var frequencies, convert to time gridpoints per year
-
-    allow to display per variable for file of given size, so can adjust scale factor (e.g. for nc compression)
-
-    '''
-
-
+    print(warning_msg)
 
 if __name__ == '__main__':
     main()
