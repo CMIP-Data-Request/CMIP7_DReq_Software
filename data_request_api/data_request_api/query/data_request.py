e3af1895d05a73c024134013015c1089eee2889e
rigoudyg <gaelle.rigoudy@meteo.fr>
2025-05-02 16:52:01 +0200

    Change warning.

--- data_request_api/data_request_api/query/data_request.py
+++ data_request_api/data_request_api/query/data_request.py
@@ -983,7 +983,8 @@ class DataRequest(object):
             tmp_rep, to_add = to_add, set()
         return rep
 
-    def filter_elements_per_request(self, elements_to_filter, requests=dict(), operation="all", skip_if_missing=False):
+    def filter_elements_per_request(self, elements_to_filter, requests=dict(), operation="all", skip_if_missing=False,
+                                    print_warning_bcv=True):
         """
         Filter the elements of kind element_type with a dictionary of requests.
         :param str or list od DRObjects elements_to_filter: kind of elements to be filtered
@@ -997,6 +998,15 @@ class DataRequest(object):
             raise ValueError(f"Operation does not accept {operation} as value: choose among 'any' (match at least one"
                              f" requirement) and 'all' (match all requirements)")
         else:
+            # Get elements corresponding to element_type
+            if isinstance(elements_to_filter, str):
+                elements = self.get_elements_per_kind(elements_to_filter)
+            else:
+                if not isinstance(elements_to_filter, list):
+                    elements = [elements_to_filter, ]
+                else:
+                    elements = elements_to_filter
+                elements_to_filter = elements[0].DR_type
             # Prepare the request dictionary
             request_dict = defaultdict(list)
             for (req, values) in requests.items():
@@ -1014,29 +1024,6 @@ class DataRequest(object):
                     else:
                         logger.error(f"Could not find value {val} for element type {req}.")
                         raise ValueError(f"Could not find value {val} for element type {req}.")
-            if len(request_dict) > 0:
-                bcv_op = self.find_element("opportunities", "Baseline Climate Variables for Earth System Modelling", default=None)
-                bcv_prio = self.find_element("priority_level", "Core", default=None)
-                request_save_bcv_filtering = set(list(request_dict)) - {"opportunities", "priority_levels",
-                                                                        "max_priority_levels"}
-                if bcv_op is None or bcv_prio is None:
-                    logger.warning("Can not check that request filtering includes baseline variables, no reference found.")
-                elif len(request_save_bcv_filtering) > 0 or\
-                        (bcv_op.DR_type in request_dict and bcv_op not in request_dict[bcv_op.DR_type]) or\
-                        (bcv_prio.DR_type in request_dict and bcv_prio not in request_dict[bcv_prio.DR_type]):
-                    logger.warning(f"Request output might not include '{bcv_op.name}'. "
-                                   "To get the complete list associated with baseline variables, skip every filtering "
-                                   f"requirements except either 'opportunities={bcv_op.name}' or "
-                                   f"'priority_level={bcv_prio.name}'.")
-            # Get elements corresponding to element_type
-            if isinstance(elements_to_filter, str):
-                elements = self.get_elements_per_kind(elements_to_filter)
-            else:
-                if not isinstance(elements_to_filter, list):
-                    elements = [elements_to_filter, ]
-                else:
-                    elements = elements_to_filter
-                elements_to_filter = elements[0].DR_type
             # Filter elements
             rep = defaultdict(lambda: defaultdict(set))
             elements_filtering_structure = self.get_filtering_structure(elements_to_filter)
@@ -1089,8 +1076,16 @@ class DataRequest(object):
                         rep_list = rep_list & rep[req][val]
             else:
                 raise ValueError(f"Unknown value {operation} for operation (only 'all' and 'any' are available).")
-            rep_list = sorted(list(rep_list))
-            return rep_list
+            if print_warning_bcv and elements_to_filter in ["variables", ]:
+                bcv_list = self.find_element("opportunities", "Baseline Climate Variables for Earth System Modelling", default=None)
+                if bcv_list is None:
+                    logger.warning("Can not check that request filtering includes baseline variables, no reference found.")
+                else:
+                    bcv_list = set(bcv_list.get_variables())
+                    common_list = bcv_list & rep_list
+                    if len(common_list) < len(bcv_list):
+                        logger.warning("Output of the current filtering request does not include all the BCV variables.")
+            return sorted(list(rep_list))
 
     def find_opportunities(self, operation="any", skip_if_missing=False, **kwargs):
         """
@@ -1216,14 +1211,16 @@ class DataRequest(object):
             DR_type = columns_datasets[0].DR_type
             for (column_data, column_title) in zip(columns_datasets, columns_title_list):
                 filter_line_datasets = self.filter_elements_per_request(elements_to_filter=sorted_filtered_data,
-                                                                        requests={DR_type: column_data}, operation="all")
+                                                                        requests={DR_type: column_data},
+                                                                        operation="all", print_warning_bcv=False)
                 for line in filter_line_datasets:
                     content[lines_title_dict[line.id]][column_title] = "x"
         else:
             DR_type = sorted_filtered_data[0].DR_type
             for (line_data, line_title) in zip(sorted_filtered_data, lines_title_list):
                 filtered_columns = self.filter_elements_per_request(elements_to_filter=columns_datasets,
-                                                                    requests={DR_type: line_data}, operation="all")
+                                                                    requests={DR_type: line_data}, operation="all",
+                                                                    print_warning_bcv=False)
                 content[line_title] = {columns_title_dict[elt.id]: "x" for elt in filtered_columns}
 
         logger.debug("Format summary")
