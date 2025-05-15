2d570a2e92e6bf5ddbcf915461fb4bdba0be93da
James Anstey <james.anstey@ec.gc.ca>
2025-05-15 11:04:10 +0000

    added check for latest pypi API version

--- data_request_api/data_request_api/utilities/config.py
+++ data_request_api/data_request_api/utilities/config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-
+from importlib.metadata import version, PackageNotFoundError
 import os
 from pathlib import Path
-
+import requests
 import yaml
 
 # Config file location in the user's home directory
@@ -150,3 +150,38 @@ def update_config(key, value):
         yaml.dump(CONFIG, f)
 
     print(f"Updated {key} to {value} in '{CONFIG_FILE}'.")
+
+
+def check_api_version():
+    """
+    Check pypi for latest release of the software.
+    Warn user if the installed version is not the latest release.
+    """
+    try:
+        installed_version = version(PACKAGE_NAME)
+    except PackageNotFoundError:
+        print(f"{PACKAGE_NAME} is not installed.")
+        return
+
+    try:
+        response = requests.get(f"https://pypi.org/pypi/{PACKAGE_NAME}/json", timeout=5)
+        response.raise_for_status()
+        latest_version = response.json()['info']['version']
+    except requests.RequestException as e:
+        print(f"Error checking PyPI: {e}")
+        return
+
+    if installed_version != latest_version:
+        # Warn user that installed version isn't the same as the latest pypi version
+        msg =  f"Warning: installed {PACKAGE_NAME} version does not match latest pypi version\n"
+        msg += f"  Latest pypi version: {latest_version}\n"
+        msg += f"  Installed version:   {installed_version}\n"
+        msg +=  "  To install the latest version from pypi:\n"
+        msg += f"    pip install --upgrade {PACKAGE_NAME}"
+
+        # Add color to the warning message
+        color_code = "\033[91m"
+        color_code_end = color_code.split("[")[0] + "[0m"
+        msg = color_code + msg + color_code_end
+
+        print(msg)
